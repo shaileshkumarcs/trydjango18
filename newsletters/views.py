30bd801fb21a177a47e27ffd695b74dad735b354
shailesh <shailesh.kumarcs@gmail.com>
2018-04-22 18:18:51 +0530

    Custom form

--- newsletters/views.py
+++ newsletters/views.py
@@ -31,11 +31,21 @@ def home(request):
 
     return render(request, "home.html", context)
 
+
 def contact(request):
     form = ContactForm(request.POST or None)
-
+    if form.is_valid():
+        # for key in form.cleaned_data:
+        #     print(key)
+        #     print(form.cleaned_data.get(key))
+        for key, value in form.cleaned_data.items():
+            print(key, value)
+        # email = form.cleaned_data.get("email")
+        # message = form.cleaned_data.get("message")
+        # full_name = form.cleaned_data.get("full_name")
+        # print(email, message, full_name)
     context = {
-
+        "form":form
     }
 
     return render(request, "forms.html", context)
--- newsrc/urls.py
+++ newsrc/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 urlpatterns = [
     # Examples:
      url(r'^$', 'newsletters.views.home', name='home'),
+    url(r'^contact/$', 'newsletters.views.contact', name='contact'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
