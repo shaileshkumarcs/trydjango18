4b675cc7ab276235cbb7e06ce063d7fbcbc9c76d
shailesh <shailesh.kumarcs@gmail.com>
2018-04-22 17:55:19 +0530

    Model Form validation and custom form validation as well as

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,5 +1,41 @@
 from django.shortcuts import render
 
+from .forms import ContactForm,SignUpForm
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {})
+    title = 'Welcome'
+
+    form = SignUpForm(request.POST or None)
+
+    context = {
+        "title": title,
+        "form": form
+    }
+
+
+    if form.is_valid():
+        #form.save()
+        instance = form.save(commit=False)
+        full_name = form.cleaned_data.get("full_name")
+        if not full_name:
+            full_name = "New full name"
+        instance.full_name = full_name
+
+        # if not instance.full_name:
+        #     instance.full_name = "Shailesh"
+        instance.save()
+        title = "Thank You"
+        context = {
+            "title": title,
+        }
+
+    return render(request, "home.html", context)
+
+def contact(request):
+    form = ContactForm(request.POST or None)
+
+    context = {
+
+    }
+
+    return render(request, "forms.html", context)
